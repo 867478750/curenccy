56c3487cf230a8587dc611dcb1f844199b621487
nlb <867478750@qq.com>
2019-04-06 14:52:32 +0800

    add anotations

---
+++ src/main/java/org/nlb/currency/anotations/threadRecommand.java
@@ -0,0 +1,12 @@
+package org.nlb.currency.anotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.TYPE)
+public @interface threadRecommand {
+    String value() default "";
+}
